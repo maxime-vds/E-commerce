17a5f71ae470aad87fecbe70eb3f504536a6ab5c
maxime-vds <maxime.vandriessche@hotmail.be>
2023-04-24 17:46:24 +0200

    testlogin et responsive infoshop

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
+
 import "./App.css"
+import Log from "./comps/testLogin";
+
 import LandingPage from "./page/landingPage";
 
 function App() {
-  
+
+
+
 
   return (
     <>
-    <LandingPage/>
-      
-       
-      
-       
-        
-    
-        
-      
-    
+      <LandingPage/>
 
-
-
-      
+      {/* <Log /> */}
     </>
   );
 }
