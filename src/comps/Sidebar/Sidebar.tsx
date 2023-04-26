ef91a79469950950dfc021be7a825c59d86c1035
Ricky <jacobus.kim@gmail.com>
2023-04-26 16:55:46 +0200

    sidebar

--- src/comps/Sidebar/Sidebar.tsx
+++ src/comps/Sidebar/Sidebar.tsx
@@ -1,18 +1,17 @@
+// type SideBarProps = {
+//     selectSidebar: boolean
+//     setSelectSidebar: React.Dispatch<React.SetStateAction<boolean>>
+// }
 
-type SideBarProps = {
-    selectSidebar: boolean
-    setSelectSidebar: React.Dispatch<React.SetStateAction<boolean>>
-}
+// function SideBar({selectSidebar, setSelectSidebar}:SideBarProps) {
+//     return (  
+//         //select classnames on boolean
+//         <div onClick={() => setSelectSidebar(false)} 
+//         className={selectSidebar ? 
+//         "absolute bg-slate-200  transition-all duration-300 ease-in-out w-screen h-0 h-screen z-10" 
+//         : ""}>
+//         </div>
+//     );
+// }
 
-function SideBar({selectSidebar, setSelectSidebar}:SideBarProps) {
-    return (  
-        //select classnames on boolean
-        <div onClick={() => setSelectSidebar(false)} 
-        className={selectSidebar ? 
-        "absolute bg-slate-200  transition-all duration-300 ease-in-out w-screen h-0 h-screen z-10" 
-        : ""}>
-        </div>
-    );
-}
-
-export default SideBar;
+// export default SideBar;
