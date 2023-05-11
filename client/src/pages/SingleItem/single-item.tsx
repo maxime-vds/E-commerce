165e1353274d5678c247d124b24b9716611943b5
Ricky <jacobus.kim@gmail.com>
2023-05-11 16:40:19 +0200

    single-item page

--- client/src/pages/SingleItem/single-item.tsx
+++ client/src/pages/SingleItem/single-item.tsx
@@ -12,19 +12,21 @@ type SingleItemProps = {
 export const SingleItem = ({ data, setSelectModal, AddToUserCart }: SingleItemProps) => {
   const { id = 0 }: any = useParams()
   return (
-    <div className="SingleItem flex flex-col">
-      <div className="item-wrapper mt-9" onClick={() => setSelectModal(true)}>
-        <img className="w-96" src={data[id - 1].imageURL}></img>
-      </div>
-      <div className="flex flex-col space-y-3 item-description w-60 ms-10 mt-10">
+    <div className="SingleItem flex flex-col min-[860px]:flex-row justify-center pt-5 ">
+      <img
+        className="w-auto pb-10 min-[860px]:h-[40rem] outline outline-1 mx-1"
+        onClick={() => setSelectModal(true)}
+        src={data[id - 1].imageURL}
+      />
+      <div className="item-description flex flex-col space-y-3 m-3">
         <p className="font-compagnonMed text-xl">{data[id].description}</p>
-        <div className="price-wrapper flex space-x-5">
+        <div className="price-wrapper pb-3 flex space-x-5">
           <p className="font-nimbus text-2xl">€{data[id].price}</p>
           <p className="font-nimbus text-sm pt-2">including VAT</p>
         </div>
 
         <div onClick={() => (AddToUserCart(id, data[id].price, data[id].imageURL), console.log('you clicked buy'))}>
-          <h2 className="font-lacklineReg cursor-pointer outline outline-1 text-center text-white text-xl  p-2 bg-black hover:underline hover:bg-slate-100 hover:text-black">
+          <h2 className="font-lacklineReg cursor-pointer outline outline-1 text-center text-white text-xl p-2 bg-black hover:underline hover:bg-slate-100 hover:text-black">
             Buy now!
           </h2>
         </div>
